27280ba71c7a95ce91a2c2367c958f992c7c7486
Ruby Banga <uic.ruby@gmail.com>
2018-09-29 11:15:15 -0700

    fixed up https error

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,8 +3,7 @@ var topics = ['Skunks', 'Elephant', 'Snake', 'Tiger', 'Cheetah', 'Lion', 'Monkey
 // a function to create a button of all elements in array
 function button() {
     $('#panel').empty();
-    for (var i = 0; i < topics.length; i++)
-     {
+    for (var i = 0; i < topics.length; i++) {
         var button = $('<button>');
         button.addClass('animalButton');
         button.attr('data-name', topics[i]);
@@ -12,7 +11,45 @@ function button() {
         $("#panel").prepend(button);
     }
     addgif();
+    //addSound();
 };
+function addSound() {
+
+    $('#submit').on('click', function (e) {
+        e.preventDefault();
+        //    var p=$(this).data('name');
+        //    var query    ="GET https://www.googleapis.com/youtube/v3/search" + p + "AIzaSyDj8nQkKtYUX1DyaGo6E43_gywHp4xWoSY";
+
+        //    $.ajax({
+        //     url: query,
+        //     method: 'GET',
+        //     part: 'snippet',
+        //     p: 'surfing',
+        //     type:'video'
+        // })
+        // .done(function(responseSong){
+        // var resultSong=responseSong.data;
+        // console.log(resultSong);
+        var chanelName='uic.ruby';
+        var request = gapi.client.youtube.search.list({
+            q: encodeURIComponent($("#animal_input").val()).replace(/%20/g, "+"),
+            order: 'viewcount',
+            part: 'snippet',
+            type: 'video',
+            maxResults: 3,
+            publishedAfter: "2015-01-01T00:00:00Z",
+            forUsername: chanelName
+
+        });
+request.execute(function(response){
+console.log(response);
+});
+
+    });
+}
+addSound();
+
+
 
 // made a function to display giphy's
 function addgif() {
@@ -27,7 +64,7 @@ function addgif() {
         console.log(p);
 
         // api is stored in variable queryURL
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + p + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + p + "&api_key=dc6zaTOxFJmzC&limit=10";
 
         $.ajax({
             url: queryURL,
@@ -95,7 +132,8 @@ function addgif() {
         $('#giphy').empty();  //will replace the exixting giphy's on the page
     });
 }
-$('#submit').on('click', function () {
+$('#submit').on('click', function (event) {
+    // event.preventDefault();  //you can use this instead of return false too.
     var input = $('#animal_input').val().trim();
     topics.push(input);
     button();
